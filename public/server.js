acc1412ae17118491578f9c5cc73f4adf626d788
James McEwen <james@silverfoxmedia.co>
2025-04-20 21:15:24 -0500

    Update server.js

--- public/server.js
+++ public/server.js
@@ -17,6 +17,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+
 
 
 //Routes
