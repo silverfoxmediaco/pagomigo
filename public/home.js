7fbd4b0b97747985d32d9580765448f3ce53bfad
James McEwen <james@silverfoxmedia.co>
2025-04-23 16:02:30 -0500

    Update home.js

--- public/home.js
+++ public/home.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const password = document.getElementById("password").value;
   
         try {
-          const res = await fetch("/api/auth/signup", {
+          const res = await fetch("/api/auth/register", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, username, phone, password })
