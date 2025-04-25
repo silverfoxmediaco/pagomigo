0661605956d5d89b718eb79247541ca7b3d94250
James McEwen <james@silverfoxmedia.co>
2025-04-25 15:10:17 -0500

    Update login.js

--- public/login.js
+++ public/login.js
@@ -140,3 +140,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// Burger menu logic
+const hamburger = document.getElementById("hamburger");
+const slideoutMenu = document.getElementById("slideoutMenu");
+const closeBtn = document.getElementById("closeMenu");
+
+if (hamburger) {
+  hamburger.addEventListener("click", () => {
+    slideoutMenu.classList.add("open");
+  });
+}
+
+if (closeBtn) {
+  closeBtn.addEventListener("click", () => {
+    slideoutMenu.classList.remove("open");
+  });
+}
