7441b465209aa8367245a56665520c5fdb10f079
James McEwen <james@silverfoxmedia.co>
2025-04-27 18:51:46 -0500

    Update server.js

--- server.js
+++ server.js
@@ -49,6 +49,62 @@ app.use((req, res, next) => {
 // In Express server.js or app.js
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') return next(); 
+
+  const ignorePaths = ['/favicon.ico', '/robots.txt', '/sitemap.xml'];
+  if (ignorePaths.includes(req.path)) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/api/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/public/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/static/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/assets/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/images/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/css/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/js/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/fonts/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/videos/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/audio/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/docs/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/uploads/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/downloads/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/api/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/api/v1/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/api/v2/')) {
+    return res.status(200).send('OK');
+  }
+  if (req.path.startsWith('/api/v3/')) {
+    return res.status(200).send('OK');
+  }
   
   if (req.hostname === 'pagomigo.com') {  
     return res.redirect(301, `https://www.pagomigo.com${req.originalUrl}`);
