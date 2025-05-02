5edef02f4e397a904c308d45372664527d23e72a
James McEwen <james@silverfoxmedia.co>
2025-05-02 14:39:31 -0500

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,17 @@ app.use(session({
     ttl: 14 * 24 * 60 * 60, // 14 days
   }),
 
-  app.get('/test-cookie', (req, res) => {
+    cookie: {
+    httpOnly: true,
+    secure: isProd,
+    sameSite: isProd ? 'none' : 'lax',
+    
+    domain: isProd ? '.pagomigo.com' : undefined,
+    maxAge: 24 * 60 * 60 * 1000 // 1 day
+  }
+}));
+
+app.get('/test-cookie', (req, res) => {
   res.cookie('connect.sid', 'test-session-value', {
     httpOnly: true,
     secure: isProd,
@@ -44,17 +54,6 @@ app.use(session({
 
   res.send('Test cookie has been set.');
 });
-
-  
-  /*cookie: {
-    httpOnly: true,
-    secure: isProd,
-    sameSite: isProd ? 'none' : 'lax',
-    
-    domain: isProd ? '.pagomigo.com' : undefined,
-    maxAge: 24 * 60 * 60 * 1000 // 1 day
-  }
-}));*/
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
